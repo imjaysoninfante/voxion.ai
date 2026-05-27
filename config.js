d6268afeb92b6a79aabb2e84066bd98116a940a4
Jayson Infante <im.jaysoninfante@gmail.com>
2026-05-27 11:45:38 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    appId: "YOUR_AGORA_APP_ID",  // Get from console.agora.io
+        channel: "voxion-channel",
+        token: null,  // Use null for testing, add token for production
+      };
